340cf758eadd0e44f732974c4309007a5d94e103
Shuai Wang <277668922@qq.com>
2024-03-15 11:53:08 +0800

    修改缓存区为测试缓存区

--- fal_cfg.h
+++ fal_cfg.h
@@ -2,8 +2,8 @@
  * @Author: Shuai Wang 277668922@qq.com
  * @Date: 2023-12-25 14:15:48
  * @LastEditors: Shuai Wang 277668922@qq.com
- * @LastEditTime: 2024-01-04 14:16:25
- * @FilePath: \stm32f407zet6\APP\FAL\fal_cfg.h
+ * @LastEditTime: 2024-03-13 15:52:34
+ * @FilePath: \Demo_LED\Library\fal\fal_cfg.h
  * @Description:
  */
 /*
@@ -37,16 +37,21 @@ extern struct fal_flash_dev nor_flash0;
 /* ====================== Partition Configuration ========================== */
 #ifdef FAL_PART_HAS_TABLE_CFG
 /* partition table */
-#define FAL_PART_TABLE                                                                                   \
-    {                                                                                                    \
-        {FAL_PART_MAGIC_WORD, "Reserved", NOR_FLASH_DEV_NAME, 0, 16 * 1024, 0},                          \
-            {FAL_PART_MAGIC_WORD, "ConfigsPart", NOR_FLASH_DEV_NAME, 16 * 1024, 32 * 1024, 0},           \
-            {FAL_PART_MAGIC_WORD, "FontsConfigPart", NOR_FLASH_DEV_NAME, 48 * 1024, 32 * 1024, 0},       \
-            {FAL_PART_MAGIC_WORD, "PatientPart", NOR_FLASH_DEV_NAME, 80 * 1024, 32 * 1024, 0},       \
-            {FAL_PART_MAGIC_WORD, "StatisticPart", NOR_FLASH_DEV_NAME, 112 * 1024, 64 * 1024, 0},       \
-            {FAL_PART_MAGIC_WORD, "Fonts", NOR_FLASH_DEV_NAME, 520 * 1024, 1024 * 1024, 0},              \
-            {FAL_PART_MAGIC_WORD, BSCIC_CONFIG_NAME, NOR_FLASH_DEV_NAME, 2 * 1024 * 1024, 16 * 1024, 0}, \
+#define FAL_PART_TABLE                                                                        \
+    {                                                                                         \
+        {FAL_PART_MAGIC_WORD, "StatisticPart", NOR_FLASH_DEV_NAME, 1024*1024, 8* 0x1000, 0}, \
     }
+
+// {                                                                                                    \
+    //     {FAL_PART_MAGIC_WORD, "Reserved", NOR_FLASH_DEV_NAME, 0, 16 * 1024, 0},                          \
+    //         {FAL_PART_MAGIC_WORD, "ConfigsPart", NOR_FLASH_DEV_NAME, 16 * 1024, 32 * 1024, 0},           \
+    //         {FAL_PART_MAGIC_WORD, "FontsConfigPart", NOR_FLASH_DEV_NAME, 48 * 1024, 32 * 1024, 0},       \
+    //         {FAL_PART_MAGIC_WORD, "PatientPart", NOR_FLASH_DEV_NAME, 80 * 1024, 32 * 1024, 0},       \
+    //         {FAL_PART_MAGIC_WORD, "StatisticPart", NOR_FLASH_DEV_NAME, 112 * 1024, 64 * 1024, 0},       \
+    //         {FAL_PART_MAGIC_WORD, "Fonts", NOR_FLASH_DEV_NAME, 520 * 1024, 1024 * 1024, 0},              \
+    //         {FAL_PART_MAGIC_WORD, BSCIC_CONFIG_NAME, NOR_FLASH_DEV_NAME, 2 * 1024 * 1024, 16 * 1024, 0}, \
+    // }
+
 #endif /* FAL_PART_HAS_TABLE_CFG */
 
 #endif /* _FAL_CFG_H_ */
